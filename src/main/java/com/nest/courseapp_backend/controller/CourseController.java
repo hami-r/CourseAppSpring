a826749de53404a9605c81e1388332cd7105d22b
hami-r <mynameissaitama54@gmail.com>
2022-12-06 11:34:26 +0530

    add page

--- src/main/java/com/nest/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/nest/courseapp_backend/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.nest.courseapp_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class CourseController {
     @GetMapping("/view")
     public String  viewCourse(){
         return "Welcome to view course page";
+    }
+
+    @PostMapping("/add")
+    public String addCourse() {
+        return "Course added successfully";
     }
 }
