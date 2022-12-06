c827cdd86b534e245d7d0d68a6dff3ef75d5e378
hami-r <mynameissaitama54@gmail.com>
2022-12-06 11:54:26 +0530

    model class created

--- src/main/java/com/nest/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/nest/courseapp_backend/controller/CourseController.java
@@ -1,7 +1,9 @@
 package com.nest.courseapp_backend.controller;
 
+import com.nest.courseapp_backend.model.CourseModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +19,13 @@ public class CourseController {
         return "Welcome to view course page";
     }
 
-    @PostMapping("/add")
-    public String addCourse() {
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public String addCourse(@RequestBody CourseModel c) {
+        System.out.println("Title:" + c.getCourseTitle() + "\n" +
+                "Description:" + c.getCourseDescription() + "\n" +
+                "Venue:" + c.getCourseVenue() + "\n" +
+                "Duration:" + c.getCourseDuration() + "\n" +
+                "Date:" + c.getCourseDate());
         return "Course added successfully";
     }
 }
