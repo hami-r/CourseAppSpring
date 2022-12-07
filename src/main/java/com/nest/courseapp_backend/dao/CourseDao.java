1d549e1873f3e8b92d46ff5f88508eeda27d9e08
hami-r <mynameissaitama54@gmail.com>
2022-12-07 19:01:59 +0530

    text changed

--- src/main/java/com/nest/courseapp_backend/dao/CourseDao.java
+++ src/main/java/com/nest/courseapp_backend/dao/CourseDao.java
@@ -1,13 +1,12 @@
 package com.nest.courseapp_backend.dao;
 
 import com.nest.courseapp_backend.model.CourseModel;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CourseDao extends CrudRepository<CourseModel,Integer> {
-    @Query(value = "SELECT `id`,`course_date`,`course_description`,`course_duration`,`course_title`,`course_venue` FROM `courses` WHERE `course_title` = :title",nativeQuery = true)
+    @Query(value = "SELECT `id`,`course_date`,`course_description`,`course_duration`,`course_title`,`course_venue` FROM `courses` WHERE `course_title` LIKE  %:title%",nativeQuery = true)
     List<CourseModel> searchCourse(String title);
 }
